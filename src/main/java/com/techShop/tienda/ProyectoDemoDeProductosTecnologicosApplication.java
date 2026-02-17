4960aa2fc8bb3978952e0ceba0a389236dd0746a
Estiven123host <castros.0302@gmail.com>
2026-02-16 23:53:05 -0600

    Primer commit - Proyecto Java base

---
+++ src/main/java/com/techShop/tienda/ProyectoDemoDeProductosTecnologicosApplication.java
@@ -0,0 +1,13 @@
+package com.techShop.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDemoDeProductosTecnologicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDemoDeProductosTecnologicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techShop/tienda/ProyectoDemoDeProductosTecnologicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techShop.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDemoDeProductosTecnologicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
